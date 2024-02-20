ad4a06ee520384aa07b0575fa4b699aceee71c8e
yugal-kishore143 <66868933+yugal-kishore143@users.noreply.github.com>
2024-02-20 14:52:23 +0530

    update time format in js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const schedule = [
-    { time: '09:00', event: 'Opening Ceremony', description: 'The inaugration is about to begin, click here to navigate.' },
-    { time: '12:00', event: 'Lunch Time', description: 'Time to grab some lunch and network.' },
-    { time: '15:00', event: 'Evaluation Time', description: 'Time for evaluations and feedback.' },
-    { time: '18:00', event: 'Closing Ceremony', description: 'The event concludes with the closing ceremony.' }
+    { time: '09:00', event: 'Opening Ceremony', description: 'hello,The inaugration is about to begin, click here to navigate.' },
+    { time: '02:00', event: 'Lunch Time', description: 'Time to grab some lunch and network.' },
+    { time: '01:00', event: 'Evaluation Time', description: 'Time for evaluations and feedback.' },
+    { time: '06:00', event: 'Closing Ceremony', description: 'The event concludes with the closing ceremony.' }
 ];
 
 function updateSchedule() {
